8a6fff7d60c98dd08176700171d0f97ac36c8f41
Guest User <guestuser@atc.local>
2021-01-15 08:15:18 +0100

    javascript

---
+++ js/script.js
@@ -0,0 +1,64 @@
+
+var num = 378;
+
+console.log(num);
+
+var greeting = "Jam ne Javascript!";
+console.log(greeting);
+
+
+//alert(greeting)
+
+/* ky 
+eshte nje koment */
+
+var x = 10;
+var y = 4;
+var linebreak = "<br>";
+
+
+
+document.write(linebreak);
+
+
+document.write("x + y = ");
+
+document.write(x+y);
+document.write(linebreak);
+
+document.write("x - y = ");
+
+document.write(x-y);
+document.write(linebreak);
+
+
+document.write("x * y = ");
+
+document.write(x*y);
+document.write(linebreak);
+
+document.write("x / y = ");
+
+document.write(x/y);
+document.write(linebreak);
+
+document.write("x % y = ");
+
+document.write(x%y);
+document.write(linebreak);
+
+//&& dhe || 
+
+
+var a = true;
+var b = false;
+
+console.log(a && b) //nxjerr false
+console.log(a || b) //nxjerr true
+console.log(!a && b) //nxjerr false
+console.log(!a || b) //nxjerr false
+console.log((!a || b) || (a && !b)) //nxjerr true
+
+
+
+
